a49b1fd86d84137ac5fd9c789abb36095be91794
CHA0S14 <chaoscatorce14@gmail.com>
2018-03-12 20:35:31 +0100

    Comentada la tabal de palabras reservadas

--- src/analizadorLexico/tablas/TablaPalabrasReservadas.java
+++ src/analizadorLexico/tablas/TablaPalabrasReservadas.java
@@ -1,7 +1,8 @@
 package analizadorLexico.tablas;
 
 /**
- *  
+ * Clase que contiene la tabla de palabras reservadas del lenguaje
+ * 
  * @author Ismael Ortega Sanchez
  *
  */
@@ -9,14 +10,25 @@ public class TablaPalabrasReservadas {
 	private final String[] palabrasReservadas = { "true", "false", "var", "int", "bool", "chars", "write", "promp",
 			"return", "if", "else", "while", "do", "for", "switch", "case", "break", "default", "funtion" };
 
+	/**
+	 * Metodo que devuelve la codificacion del token para la palabra reservada o -1
+	 * si no es una palbra reservada
+	 * 
+	 * @param palabraReservada
+	 *            Cadena que se desea comprobar si es una palabra reservada
+	 * @return Codigo del token para la palabra reservada o -1 si no existe
+	 */
 	public int getCodigoPalabraReservada(String palabraReservada) {
 		int index = -1;
 		int i = 0;
+		// Recorremos las tabla en busca de la palabra reservada
 		while (index == -1 && i < palabrasReservadas.length) {
-			if(palabrasReservadas[i].equals(palabraReservada))
+			if (palabrasReservadas[i].equals(palabraReservada))
 				index = i;
 			i++;
 		}
+		// Si se ha encontrado algo se le suma 27 al indice para codificar el token
+		// La primera paralabra reservada, true es el token 27 el indice es 0
 		return index != -1 ? 27 + index : index;
 	}
 }
